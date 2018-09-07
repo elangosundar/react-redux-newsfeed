198cab5de6dee55ea5e88167d08762e4ba5290f6
elangovan.sundar <elangovan2me@gmail.com>
2018-09-07 19:13:46 +0530

    Simple Counter functionality added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from "react";
-import ReactDOM from "react-dom";
-const App = () => {
+import style from './main.css';
+
+/*const App = () => {
   return (
     <div>
-      <p>Welcome to dReact here!</p>
+      <p>Welcome to React here!</p>
     </div>
   );
-};
+}*/
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count : 0
+    }
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  
+  increment() {
+    this.setState(
+      {count : this.state.count+1}
+    );
+  }
+  
+  decrement() {
+    this.setState(
+      {count : this.state.count-1}
+    );
+  }
+
+  render() {  
+     return (
+        <div>
+          <h1>Simple Counter</h1>
+          <div>
+              <button onClick={this.increment} > + </button>
+              <span><b>Count:</b> { this.state.count } </span>
+              <button onClick={this.decrement} > - </button>
+          </div>
+        </div>
+     );
+  }
+}
+
 export default App;
-ReactDOM.render(<App />, document.getElementById("app"));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
-import style from "./main.css";
 
-console.log(`I'm a entry point`);
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
-
+ReactDOM.render(<App />, document.getElementById("app"));
